9e1c5329dc807a23f783e25a09af38083fa7d025
Isaac <isaac.vazquez@iteso.mx>
2024-11-24 23:24:07 -0600

    added user services

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -11,7 +11,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_has(password):
+def get_password_hash(password):
     return pwd_context.hash(password)
 
 def create_acces_token(data: dict, expires_delta: Optional[timedelta]= None):
