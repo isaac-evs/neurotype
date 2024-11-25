bcf9e4feac1b4d88398c26803f7509593a484a0a
Isaac <isaac.vazquez@iteso.mx>
2024-11-24 23:08:36 -0600

    added auth

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,3 +1,22 @@
+from warnings import deprecated
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from typing import Optional
+from app.schemas.user import UserInDB
+from app.core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_has(password):
+    return pwd_context.hash(password)
+
+def create_acces_token(data: dict, expires_delta: Optional[timedelta]= None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
