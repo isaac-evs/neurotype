3ba22d05dff4ff562eaf7e05ed1e3000c7f274a8
Isaac <isaac.vazquez@iteso.mx>
2024-11-25 21:08:20 -0600

    added venv for security

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "NeuroType"
-    SQLALCHEMY_DATABASE_URI: str = "postgresql://postgres:rootpostgrespassword@database1.cnmigicey46e.us-east-2.rds.amazonaws.com:5432/postgres"
-    SECRET_KEY: str = "t7lzOFk41j736Pmja"
+    SQLALCHEMY_DATABASE_URI: str
+    SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     ALGORITHM: str = "HS256"
 
